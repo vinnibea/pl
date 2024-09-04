59973bf22456f5e13312e36110567602a4f0d197
Roman Marunkevich <vinnibea@gmail.com>
2024-09-04 06:35:14 +0300

    fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,15 +3,14 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     const { isAuth } = storeToRefs(useLocalUserStore());
     const localStore = useLocalUserStore();
-  
+   console.log(to)
     if (!isAuth.value) {
         $fetch('/api/profile').then(res => {
             localStore.setLocalUser(res);
-            return navigateTo(to)
+            return navigateTo(to.fullPath)
         }).catch(() => {
             return navigateTo('/')
         })
 
     }
-        
 })
