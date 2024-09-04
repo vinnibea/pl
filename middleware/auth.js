6b24dd960934378d19f7e2409e0b70b9ab974933
Roman Marunkevich <vinnibea@gmail.com>
2024-09-04 03:07:19 +0300

    sol

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,21 +4,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     const { isAuth } = storeToRefs(useLocalUserStore());
     const localStore = useLocalUserStore();
-    if (from.fullPath === to.fullPath || !isAuth && to.fullPath === '/account') {
-        $fetch("/api/")
-            .then((res) => {
-                if (res.statusCode > 200) {
-                    localStore.setLocalUser()
-                    return navigateTo('/')
-                }
-                if (res.id) {
-                    localStore.setLocalUser(res, true);
-                }
-                return res;
-            }).catch((e) => {
-                console.warn(e.statusCode, 'error from middleware')
-                localStore.setLocalUser()
-                return navigateTo('/')
-            })
-    };
+    if (!isAuth) {
+         navigateTo('/')
+    }
+        
 })
