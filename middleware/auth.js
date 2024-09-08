89dc94df9e340106a6ff16225f8ea4933201afc3
Roman Marunkevich <vinnibea@gmail.com>
2024-09-08 18:21:51 +0300

    fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ import { useLocalUserStore } from "~/stores/localStore";
 export default defineNuxtRouteMiddleware(async (to, from) => {
 
     const { isAuth } = storeToRefs(useLocalUserStore());
-    const localStore = useLocalUserStore();
     if (!isAuth.value && to.path === '/account') {
         return navigateTo('/register'); 
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,4 @@
 <script setup>
-import { useLocalUserStore } from "~/stores/localStore.js";
-import { useGlobalStore } from "~/stores/globalStore.js";
-const localStore = useLocalUserStore();
-const globalStore = useGlobalStore();
 const cardContent = [
   {
     title: "Регистрация на сайте",
@@ -44,23 +40,6 @@ const cardColContent = [
   },
 ];
 
-// onBeforeMount(() => {
-//   $fetch("/api/")
-//     .then((res) => {
-//       if(res.statusCode === 401) {
-//         localStore.setLocalUser()
-//         return res
-//       }
-//       if (res.id) {
-//         localStore.setLocalUser(res, true);
-//       }
-//       return res;
-//     }).catch((e) => (
-//       localStore.setLocalUser()
-//     )).finally(() => {
-//       globalStore.setLoading(false)
-//     });
-// });
 </script>
 
 <template>
