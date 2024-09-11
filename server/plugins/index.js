92633052b9b874134253f5ab15560af2c7a19ad2
Roman Marunkevich <vinnibea@gmail.com>
2024-09-11 20:06:06 +0300

    ping

--- server/plugins/index.js
+++ server/plugins/index.js
@@ -6,45 +6,45 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 const URI = useRuntimeConfig().mongoUrl;
-// const mongoose_runner = async () => {
-//   if (cached.conn) {
-//     return cached.conn;
-//   }
+const mongoose_runner = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
   const options = {
     serverSelectionTimeoutMS: 10000, // 30 секунд
-    socketTimeoutMS: 50000, // 45 секунд
-    minPoolSize: 15,
-    maxPoolSize: 80,
+    socketTimeoutMS: 5000, // 45 секунд
+    minPoolSize: 1,
+    maxPoolSize: 30,
   }
 
-//   cached.promise = mongoose.connect(URI, options).then(connection => connection);
-//   cached.connection = await cached.promise;
-// }
+  cached.promise = mongoose.connect(URI, options).then(connection => connection);
+  cached.connection = await cached.promise;
+}
 
-// const ping = () => {
-//   setInterval(async () => {
-//     try {
-//       if (mongoose.connection.readyState === 1) {
-//         await mongoose.connection.db.admin().ping();
-//         console.log('db pinged successfully')
-//       } else {
-//         console.warn('DB connection was lost');
-//         console.log('DB connection trying to establish new connection');
-//         await mongoose_runner();
-//       }
-//     } catch (e) {
-//       console.warn(e);
-//       try {
-//         console.log('Retrying to establish connection')
-//         await mongoose_runner();
-//       } catch (e) {
-//         console.warn('Failed')
-//       }
-//     } 
-//   }, 1000 * 60 * 5);
+const ping = () => {
+  setInterval(async () => {
+    try {
+      if (mongoose.connection.readyState === 1) {
+        await mongoose.connection.db.admin().ping();
+        console.log('db pinged successfully')
+      } else {
+        console.warn('DB connection was lost');
+        console.log('DB connection trying to establish new connection');
+        await mongoose_runner();
+      }
+    } catch (e) {
+      console.warn(e);
+      try {
+        console.log('Retrying to establish connection')
+        await mongoose_runner();
+      } catch (e) {
+        console.warn('Failed')
+      }
+    } 
+  }, 1000 * 60 * 5);
 
-// }
+}
 
 
 async function connectToDatabase() {
@@ -57,26 +57,27 @@ async function connectToDatabase() {
   }
 }
 
-  export default defineNitroPlugin((nitroApp) => {
+  export default defineNitroPlugin(async (nitroApp) => {
     console.log('Nitro plugin', nitroApp)
-      connectToDatabase() 
+
+    try {
+      await mongoose_runner();
+      console.log("DB connection established.");
+      ping()
+      console.log("Ping is on.");
+  
+   
+    } catch (err) {
+      console.log(err)
+      console.error("DB connection failed.", err);
+      try {
+        await mongoose_runner();
+        console.log("DB connection established.");
+  
+      } catch (e) {
+        console.log(e)
+        console.error('Not stable')
+      }
+    }
   })
-  // try {
-  //   await mongoose_runner();
-  //   console.log("DB connection established.");
-  //   ping()
-  //   console.log("Ping is on.");
-
  
-  // } catch (err) {
-  //   console.log(err)
-  //   console.error("DB connection failed.", err);
-  //   try {
-  //     await mongoose_runner();
-  //     console.log("DB connection established.");
-
-  //   } catch (e) {
-  //     console.log(e)
-  //     console.error('Not stable')
-  //   }
-  // }
