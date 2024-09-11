2df04a9bd6ec0cfdd4651fcd4ca4580b954c0d15
Roman Marunkevich <vinnibea@gmail.com>
2024-09-11 10:28:41 +0300

    ping

--- server/plugins/index.js
+++ server/plugins/index.js
@@ -12,58 +12,69 @@ const mongoose_runner = async () => {
   }
 
   const options = {
-    serverSelectionTimeoutMS: 50000, // 30 секунд
+    serverSelectionTimeoutMS: 10000, // 30 секунд
     socketTimeoutMS: 50000, // 45 секунд
-    minPoolSize: 5,
-    maxPoolSize: 10,
+    minPoolSize: 15,
+    maxPoolSize: 80,
   }
 
-  cached.promise = mongoose.connect(URI, options).then(connection => connection);
-  cached.connection = await cached.promise;
-}
+//   cached.promise = mongoose.connect(URI, options).then(connection => connection);
+//   cached.connection = await cached.promise;
+// }
 
-const ping = () => {
-  setInterval(async () => {
-    try {
-      if (mongoose.connection.readyState === 1) {
-        await mongoose.connection.db.admin().ping();
-        console.log('db pinged successfully')
-      } else {
-        console.warn('DB connection was lost');
-        console.log('DB connection trying to establish new connection');
-        await mongoose_runner();
-      }
-    } catch (e) {
-      console.warn(e);
-      try {
-        console.log('Retrying to establish connection')
-        await mongoose_runner();
-      } catch (e) {
-        console.warn('Failed')
-      }
-    } 
-  }, 1000 * 60 * 5);
+// const ping = () => {
+//   setInterval(async () => {
+//     try {
+//       if (mongoose.connection.readyState === 1) {
+//         await mongoose.connection.db.admin().ping();
+//         console.log('db pinged successfully')
+//       } else {
+//         console.warn('DB connection was lost');
+//         console.log('DB connection trying to establish new connection');
+//         await mongoose_runner();
+//       }
+//     } catch (e) {
+//       console.warn(e);
+//       try {
+//         console.log('Retrying to establish connection')
+//         await mongoose_runner();
+//       } catch (e) {
+//         console.warn('Failed')
+//       }
+//     } 
+//   }, 1000 * 60 * 5);
 
+// }
+
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(URI, options);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB, retrying in 5 seconds', error);
+    setTimeout(connectToDatabase, 5000); // Перепідключення через 5 секунд
+  }
 }
 export default async () => {
 
-  try {
-    await mongoose_runner();
-    console.log("DB connection established.");
-    ping()
-    console.log("Ping is on.");
+  // try {
+  //   await mongoose_runner();
+  //   console.log("DB connection established.");
+  //   ping()
+  //   console.log("Ping is on.");
 
+  connectToDatabase() 
+  // } catch (err) {
+  //   console.log(err)
+  //   console.error("DB connection failed.", err);
+  //   try {
+  //     await mongoose_runner();
+  //     console.log("DB connection established.");
 
-  } catch (err) {
-    console.log(err)
-    console.error("DB connection failed.", err);
-    try {
-      await mongoose_runner();
-      console.log("DB connection established.");
-
-    } catch (e) {
-      console.log(e)
-      console.error('Not stable')
-    }
-  }
+  //   } catch (e) {
+  //     console.log(e)
+  //     console.error('Not stable')
+  //   }
+  // }
 };
