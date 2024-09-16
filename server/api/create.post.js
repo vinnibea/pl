57f9936e94403511935528e006ae02ee56f8a7ba
Roman Marunkevich <vinnibea@gmail.com>
2024-09-16 21:54:15 +0300

    sol

--- server/api/create.post.js
+++ server/api/create.post.js
@@ -1,37 +1,37 @@
 import Stripe from 'stripe';
 import jwt from 'jsonwebtoken';
 const stripe = new Stripe(useRuntimeConfig().SSK)
-export default defineEventHandler(async (event) => {
-  try {
-    const { email } = await readBody(event);
+export default defineEventHandler(async(event) => {
+   try {
+    const {email, price} = await readBody(event);
     const customer = await stripe.customers.create({
-      email,
-    });
-    // Create the subscription. Note we're expanding the Subscription's
+        email,
+    }); 
+       // Create the subscription. Note we're expanding the Subscription's
     // latest invoice and that invoice's payment_intent
     // so we can pass it to the front end to confirm the payment
     const creation_token = jwt.sign(customer.id, useRuntimeConfig().secret);
 
-    await setCookie(event, '_cid', creation_token, { httpOnly: true, maxAge: 60 * 60 * 24 });
+    await setCookie(event, '_cid', creation_token, {httpOnly: true, maxAge: 60 * 60 * 24});
     const subscription = await stripe.subscriptions.create({
-      customer: customer.id,
-      items: [{
-        price: useRuntimeConfig().price,
-      }],
-      payment_behavior: 'default_incomplete',
-      payment_settings: { save_default_payment_method: 'on_subscription' },
-      expand: ['latest_invoice.payment_intent'],
-    });
-
-
-    return ({
-      subscription_id: subscription.id,
-      secret: subscription.latest_invoice.payment_intent.client_secret,
-    });
-  } catch (error) {
-    throw createError({
-      statusCode: 400,
-      message: 'Can`t be empty'
-    })
-  }
+        customer: customer.id,
+        items: [{
+          price,
+        }],
+        payment_behavior: 'default_incomplete',
+        payment_settings: { save_default_payment_method: 'on_subscription' },
+        expand: ['latest_invoice.payment_intent'],
+      });
+      
+      
+      return ({
+        subscription_id: subscription.id,
+        secret: subscription.latest_invoice.payment_intent.client_secret,
+      });
+   } catch (error) {
+      throw createError({
+        statusCode: 400,
+        message: 'Can`t be empty'
+      })
+   } 
 })
