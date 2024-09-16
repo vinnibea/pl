816b303ab311c59e8abda7c2732dfca8fabe11b2
Roman Marunkevich <vinnibea@gmail.com>
2024-09-16 05:30:03 +0300

    without swiper with prisma

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+export default {
+  // config options
+
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser":
+        "./node_modules/.prisma/client/index-browser.js",
+    },
+  },
+};
