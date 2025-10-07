3c88b3e8bd9a8e1f8fd3cbc646f36b9ea15d1388
Roman Marunkevich <vinnibea@gmail.com>
2025-10-08 01:10:51 +0300

    paginated cards

--- server/api/cards.get.js
+++ server/api/cards.get.js
@@ -1,22 +1,34 @@
 import prisma from '~/lib/prisma';
 export default defineEventHandler(async (event) => {
     const headers = await getRequestHeaders(event)
-
+    const query = await getQuery(event);
+    console.log(query)
+    const page = parseInt((query && query.page) || '1', 10);
+    const pageSize = 5;
     if (!event.context.bot) throw createError({
-        statusCode: 400,
+        statusCode: 403,
         statusMessage: 'Forbidden',
     })
-
-
-
     try {
+        const creditors_count = await prisma.creditor.count();
+        const hasPreviousPage = page > 1 ? true : false;
+        const hasNextPage = (page * pageSize) < creditors_count ? true : false;
         const data_to_send = await prisma.creditor.findMany({
-            include: {
+            omit: {
                 blocks: true,
-            }
+                createdAt: true,
+                updatedAt: true,
+                src: true
+            },
+            take: pageSize,
+            skip: (page - 1) * pageSize,
         });
-        console.log(data_to_send)
-        return data_to_send;
+        const payload = {
+            prev: hasPreviousPage,
+            next: hasNextPage,
+            data: data_to_send,
+        }
+        return { payload };
     } catch (e) {
         throw createError({
             statusCode: 400,
